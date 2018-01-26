8a7bbe447862c9af88e2949e9654f00cec86a28f
Helkios <jaykorup@hotmail.com>
2018-01-26 15:13:12 +0100

    Empty project
    Empty project to start from

---
+++ GGJ/Intermediate/Build/Win64/UE4Editor/Inc/GGJ/GGJGameModeBase.generated.h
@@ -0,0 +1,92 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "ObjectMacros.h"
+#include "ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef GGJ_GGJGameModeBase_generated_h
+#error "GGJGameModeBase.generated.h already included, missing '#pragma once' in GGJGameModeBase.h"
+#endif
+#define GGJ_GGJGameModeBase_generated_h
+
+#define GGJ_Source_GGJ_GGJGameModeBase_h_15_RPC_WRAPPERS
+#define GGJ_Source_GGJ_GGJGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define GGJ_Source_GGJ_GGJGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAGGJGameModeBase(); \
+	friend GGJ_API class UClass* Z_Construct_UClass_AGGJGameModeBase(); \
+public: \
+	DECLARE_CLASS(AGGJGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/GGJ"), NO_API) \
+	DECLARE_SERIALIZER(AGGJGameModeBase) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define GGJ_Source_GGJ_GGJGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAGGJGameModeBase(); \
+	friend GGJ_API class UClass* Z_Construct_UClass_AGGJGameModeBase(); \
+public: \
+	DECLARE_CLASS(AGGJGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/GGJ"), NO_API) \
+	DECLARE_SERIALIZER(AGGJGameModeBase) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define GGJ_Source_GGJ_GGJGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AGGJGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AGGJGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AGGJGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AGGJGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AGGJGameModeBase(AGGJGameModeBase&&); \
+	NO_API AGGJGameModeBase(const AGGJGameModeBase&); \
+public:
+
+
+#define GGJ_Source_GGJ_GGJGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AGGJGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AGGJGameModeBase(AGGJGameModeBase&&); \
+	NO_API AGGJGameModeBase(const AGGJGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AGGJGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AGGJGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AGGJGameModeBase)
+
+
+#define GGJ_Source_GGJ_GGJGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define GGJ_Source_GGJ_GGJGameModeBase_h_12_PROLOG
+#define GGJ_Source_GGJ_GGJGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	GGJ_Source_GGJ_GGJGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	GGJ_Source_GGJ_GGJGameModeBase_h_15_RPC_WRAPPERS \
+	GGJ_Source_GGJ_GGJGameModeBase_h_15_INCLASS \
+	GGJ_Source_GGJ_GGJGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define GGJ_Source_GGJ_GGJGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	GGJ_Source_GGJ_GGJGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	GGJ_Source_GGJ_GGJGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	GGJ_Source_GGJ_GGJGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	GGJ_Source_GGJ_GGJGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID GGJ_Source_GGJ_GGJGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
